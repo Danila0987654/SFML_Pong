4110b52f8302c5caf789aeefe62a66621df490f2
dsokolov <d.sokolov@weborama.com.ru>
2023-01-30 17:50:35 +0300

    Initialize game states

--- scr/pang.cpp
+++ scr/pang.cpp
@@ -2,28 +2,50 @@
 
 sf::RenderWindow Pang::_window;
 sf::Clock Pang::_clock;
+Pang::State Pang::_state = Uninitialized;
+std::map<Pang::State, GameState *> Pang::_stateInstances;
 
 void Pang::start() {
-    if (_window.isOpen()) return;
+    if (_state != Uninitialized) return;
 
     _window.create(sf::VideoMode(Pang::SCREEN_WIDTH, Pang::SCREEN_HEIGHT,
                                  32), "Pang");
     _window.setFramerateLimit(60);
 
+    _stateInstances[Splashscreen] = new SplashscreenState();
+    _stateInstances[Splashscreen]->init();
+    _stateInstances[Menu] = new MenuState();
+    _stateInstances[Menu]->init();
+    _stateInstances[Playing] = new PlayingState();
+    _stateInstances[Playing]->init();
+    _state = Splashscreen;
 
     gameLoop();
+    _window.close();
 }
 
 void Pang::gameLoop() {
-    while (_window.isOpen()) {
+    while (_state != Exiting) {
         float timeElapsed = _clock.restart().asSeconds();
-
         _window.clear(sf::Color(255, 255, 255));
 
+        // For not changing our state during a frame
+        GameState *currentState = _stateInstances[_state];
+
+        // Handle input
         sf::Event event{};
         while (_window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) { _window.close(); }
+            if (event.type == sf::Event::Closed) { _state = Exiting; }
+            currentState->handleInput(&event);
         }
+
+        // Update entries
+        currentState->update(timeElapsed);
+
+        // Draw our new entities
+        currentState->draw(&_window);
         _window.display();
     }
 }
+
+void Pang::setState(State s) { _state = s;}
--- scr/pang.h
+++ scr/pang.h
@@ -2,20 +2,26 @@
 #define SFML_PONG_PANG_H
 
 #include <SFML/Graphics.hpp>
+#include "states/game_state.h"
+#include "states/splashscreen_state.h"
+#include "states/menu_state.h"
+#include "states/playing_state.h"
 
 class Pang {
 public:
     const static int SCREEN_WIDTH = 500;
     const static int SCREEN_HEIGHT = 400;
 
-    enum State {Unitialized, Splashscreen, Menu, Playing, Exiting};
+    enum State {Uninitialized, Splashscreen, Menu, Playing, Exiting};
 
     static void start();
     static void gameLoop();
+    static void setState(State s);
 
 private:
     static State _state;
 
+    static std::map<State, GameState*> _stateInstances;
     static sf::RenderWindow _window;
     static sf::Clock _clock;
 };
