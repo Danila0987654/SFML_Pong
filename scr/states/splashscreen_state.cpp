c65f1e4872a52ca5654134aa433c1e20a020aeca
dsokolov <d.sokolov@weborama.com.ru>
2023-01-30 18:51:23 +0300

    Added title screen

--- scr/states/splashscreen_state.cpp
+++ scr/states/splashscreen_state.cpp
@@ -1,6 +1,19 @@
 #include "splashscreen_state.h"
 
-void SplashscreenState::init() {}
-void SplashscreenState::handleInput(sf::Event *event) {}
+void SplashscreenState::init() {
+    if (!_texture.loadFromFile("../assets/splashscreen.png")) {
+        std::cout << "Error while loading assets" << "\n";
+        return;
+    }
+    _sprite.setTexture(_texture);
+}
+
+void SplashscreenState::handleInput(sf::Event *event) {
+    if (event->type == sf::Event::KeyPressed || event->type == sf::Event::MouseButtonPressed) {
+        Pang::setState(Pang::Menu);
+    }
+}
 void SplashscreenState::update(float timeElapsed) {}
-void SplashscreenState::draw(sf::RenderWindow *window) {}
+void SplashscreenState::draw(sf::RenderWindow *window) {
+    window->draw(_sprite);
+}
--- scr/states/splashscreen_state.h
+++ scr/states/splashscreen_state.h
@@ -1,7 +1,11 @@
 #ifndef SFML_PONG_SPLASHSCREEN_STATE_H
 #define SFML_PONG_SPLASHSCREEN_STATE_H
 
+#include <iostream>
+
 #include "game_state.h"
+
+#include "../pang.h"
 
 class SplashscreenState : public GameState {
 public:
@@ -9,6 +13,10 @@ public:
     void handleInput(sf::Event *event) override;
     void update(float timeElapsed) override;
     void draw(sf::RenderWindow * window) override;
+
+private:
+    sf::Texture _texture;
+    sf::Sprite _sprite;
 };
 
 #endif //SFML_PONG_SPLASHSCREEN_STATE_H
