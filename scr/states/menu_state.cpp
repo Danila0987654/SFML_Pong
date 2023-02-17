ef266afbfeaefc31d24136e2116600e832b8c53c
dsokolov <d.sokolov@weborama.com.ru>
2023-02-17 18:38:33 +0300

    Output buttons

--- scr/states/menu_state.cpp
+++ scr/states/menu_state.cpp
@@ -1,6 +1,30 @@
 #include "menu_state.h"
 
-void MenuState::init() {}
-void MenuState::handleInput(sf::Event *event) {}
+void MenuState::init() {
+    if (!_playTexture.loadFromFile("../assets/play-btn.png") ||
+    !_exitTexture.loadFromFile("../assets/exit-btn.png")) {
+        std::cout << "Error while loading assets" << "\n";
+        return;
+    }
+
+    _playSprite.setTexture(_playTexture);
+    _exitSprite.setTexture(_exitTexture);
+    _playSprite.setPosition(260,150);
+    _exitSprite.setPosition(260,200);
+}
+void MenuState::handleInput(sf::Event *event) {
+    if (event->type == sf::Event::MouseButtonPressed) {
+        int x = event->mouseButton.x;
+        int y = event->mouseButton.y;
+        if (_playSprite.getGlobalBounds().contains(x, y)) {
+            Pang::setState(Pang::Playing);
+        } else if (_exitSprite.getGlobalBounds().contains(x, y)) {
+            Pang::setState(Pang::Exiting);
+        }
+    }
+}
 void MenuState::update(float timeElapsed) {}
-void MenuState::draw(sf::RenderWindow *window) {}
+void MenuState::draw(sf::RenderWindow *window) {
+    window->draw(_playSprite);
+    window->draw(_exitSprite);
+}
--- scr/states/menu_state.h
+++ scr/states/menu_state.h
@@ -1,7 +1,10 @@
 #ifndef SFML_PONG_MENU_STATE_H
 #define SFML_PONG_MENU_STATE_H
 
+#include <iostream>
+
 #include "game_state.h"
+#include "../pang.h"
 
 class MenuState : public GameState {
 public:
@@ -9,6 +12,12 @@ public:
     void handleInput(sf::Event *event) override;
     void update(float timeElapsed) override;
     void draw(sf::RenderWindow *window) override;
+
+private:
+    sf::Texture _playTexture;
+    sf::Sprite _playSprite;
+    sf::Texture _exitTexture;
+    sf::Sprite _exitSprite;
 };
 
 #endif //SFML_PONG_MENU_STATE_H
