903c0a61b98641b131484a8f38883de813e3fbe7
dsokolov <d.sokolov@weborama.com.ru>
2023-02-21 18:37:40 +0300

    Added player
    Created textures and print it

--- scr/states/playing_state.cpp
+++ scr/states/playing_state.cpp
@@ -1,6 +1,17 @@
 #include "playing_state.h"
 
-void PlayingState::init() {}
-void PlayingState::handleInput(sf::Event *event) {}
+void PlayingState::init() {
+    if (!_player1Texture.loadFromFile("../assets/paddle.png")) {
+        std::cout << "Error while loading assets" << "\n";
+        return;
+    }
+    _player1Sprite.setTexture(_player1Texture);
+    _player1Sprite.setScale(0.5, 0.5);
+    _player1Sprite.setPosition(100, 100);
+}
+void PlayingState::handleInput(sf::Event *event) {
+}
 void PlayingState::update(float timeElapsed) {}
-void PlayingState::draw(sf::RenderWindow *window) {}
+void PlayingState::draw(sf::RenderWindow *window) {
+    window->draw(_player1Sprite);
+}
--- scr/states/playing_state.h
+++ scr/states/playing_state.h
@@ -1,5 +1,6 @@
 #ifndef SFML_PONG_PLAYING_STATE_H
 #define SFML_PONG_PLAYING_STATE_H
+#include <iostream>
 
 #include "game_state.h"
 
@@ -9,6 +10,9 @@ public:
     void handleInput(sf::Event *event) override;
     void update(float timeElapsed) override;
     void draw(sf::RenderWindow *window) override;
+private:
+    sf::Texture _player1Texture;
+    sf::Sprite _player1Sprite;
 };
 
 #endif //SFML_PONG_PLAYING_STATE_H
